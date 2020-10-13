da143bbc4bd46769d65f13dfbf0decff1b2bef1c
kagawagao <kingsongao1221@gmail.com>
2020-10-13 12:22:32 +0800

    test: fix test case

--- __tests__/plots/bullet.spec.tsx
+++ __tests__/plots/bullet.spec.tsx
@@ -1,7 +1,8 @@
+import { BulletOptions } from '@antv/g2plot'
 import { mount } from '@vue/test-utils'
 import BulletChart from '../../src/plots/bullet'
 
-const props: any = {
+const props: BulletOptions = {
   data: [
     {
       title: '满意度',
@@ -29,27 +30,10 @@ const props: any = {
     line: null,
   },
   yAxis: false,
-  // 自定义 legend
-  legend: {
-    custom: true,
-    position: 'left',
-    items: [
-      {
-        value: '实际值',
-        name: '实际值',
-        marker: { symbol: 'square', style: { fill: '#5B8FF9', r: 5 } },
-      },
-      {
-        value: '目标值',
-        name: '目标值',
-        marker: { symbol: 'line', style: { stroke: '#5B8FF9', r: 5 } },
-      },
-    ],
-  },
 }
 
 describe('BulletChart', () => {
   test('should render without crashed', () => {
-    mount(() => <BulletChart {...props} />)
+    mount(() => <BulletChart {...(props as any)} />)
   })
 })
